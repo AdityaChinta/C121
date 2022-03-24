3e8a9ff92a2783874bb6e341145b03b581c3926a
AdityaChinta <65077977+AdityaChinta@users.noreply.github.com>
2022-03-24 18:46:17 +0530

    Add files via upload

---
+++ blackscreen.py
@@ -0,0 +1,42 @@
+import cv2  
+import numpy as np  
+  
+video = cv2.VideoCapture(0) 
+image = cv2.imread("me.jpeg") 
+  
+while True: 
+  
+    ret, frame = video.read() 
+    print(frame)
+    frame = cv2.resize(frame, (640, 480)) 
+    image = cv2.resize(image, (640, 480)) 
+  
+  
+    u_black = np.array([104, 153, 70]) 
+    l_black = np.array([30, 30, 0])
+    #Open and expand the image where there is mask 1 (color)
+    mask_1 = cv2.morphologyEx(mask_1, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
+    mask_1 = cv2.morphologyEx(mask_1, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
+
+    #Selecting only the part that does not have mask one and saving in mask 2
+    
+
+    #Keeping only the part of the images without the red color 
+    #(or any other color you may choose)
+  
+
+    #Keeping only the part of the images with the red color
+    #(or any other color you may choose)
+    res_2 = cv2.bitwise_and(bg, bg, mask=mask_1)
+
+    #Generating the final output by merging res_1 and res_2
+    final_output = cv2.addWeighted(res_1, 1, res_2, 1, 0)
+    output_file.write(final_output)
+    #Displaying the output to the user
+    cv2.imshow("magic", final_output)
+    cv2.waitKey(1)
+
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
